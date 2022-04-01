d0d21e6f845e24fcc659cc9fdec47f2998e17c25
LEE HYO JEONG <hyojeong1913@naver.com>
2022-04-02 00:14:14 +0900

    스프링 부트 설정 - JPA, DB

--- src/test/java/hello/querydsl/QuerydslApplicationTests.java
+++ src/test/java/hello/querydsl/QuerydslApplicationTests.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @SpringBootTest
 @Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
