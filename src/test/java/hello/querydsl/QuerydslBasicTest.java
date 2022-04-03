186302cc9b07da8a39cf0c618ebdd246617a9f0d
LEE HYO JEONG <hyojeong1913@naver.com>
2022-04-04 00:25:08 +0900

    시작 - JPQL vs Querydsl

---
+++ src/test/java/hello/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,92 @@
+package hello.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.querydsl.entity.Member;
+import hello.querydsl.entity.QMember;
+import hello.querydsl.entity.Team;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+/**
+ * JPQL vs Querydsl 테스트 코드
+ */
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    public void before() {
+
+        // EntityManager 로 JPAQueryFactory 생성
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    /**
+     * JPQL 로 member1 조회
+     *
+     * JPQL 은 문자로 실행 시점에 오류
+     *
+     * 파라미터 바인딩 직접
+     */
+    @Test
+    public void startJPQL() {
+
+        String qlString = "SELECT m FROM Member m WHERE m.username = :username";
+
+        Member findMember = em.createQuery(qlString, Member.class)
+                                .setParameter("username", "member1")
+                                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    /**
+     * Querydsl 로 member1 조회
+     *
+     * QueryDsl 은 코드로 컴파일 시점 오류
+     *
+     * 파라미터 바인딩 자동 처리
+     */
+    @Test
+    public void startQuerydsl() {
+
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory.select(m)
+                                        .from(m)
+                                        .where(m.username.eq("member1"))
+                                        .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+}
