cb8bca0a2141aaefd2e3378b814a21842d8ea190
LEE HYO JEONG <hyojeong1913@naver.com>
2022-04-13 00:22:35 +0900

    조인 - 페치 조인

--- src/test/java/hello/querydsl/QuerydslBasicTest.java
+++ src/test/java/hello/querydsl/QuerydslBasicTest.java
@@ -425,4 +425,53 @@ public class QuerydslBasicTest {
             System.out.println("tuple = " + tuple);
         }
     }
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    /**
+     * 페치 조인 미적용
+     *
+     * 지연로딩으로 Member, Team SQL 쿼리 각각 실행
+     */
+    @Test
+    public void fetchJoinNo() {
+
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                                .selectFrom(QMember.member)
+                                .where(QMember.member.username.eq("member1"))
+                                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+    }
+
+    /**
+     * 페치 조인 적용
+     *
+     * 즉시로딩으로 Member, Team SQL 쿼리 조인으로 한번에 조회
+     *
+     * 사용 방법 : 조인 기능 뒤에 fetchJoin() 추가
+     */
+    @Test
+    public void fetchJoinUse() {
+
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                                .selectFrom(QMember.member)
+                                .join(member.team, team)
+                                .fetchJoin()
+                                .where(QMember.member.username.eq("member1"))
+                                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+    }
 }
