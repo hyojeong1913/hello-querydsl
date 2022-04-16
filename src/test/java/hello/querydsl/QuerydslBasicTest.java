5af35fd0b4afb56bf0b43cb4ae64365c6d385361
LEE HYO JEONG <hyojeong1913@naver.com>
2022-04-16 23:33:03 +0900

    상수, 문자 더하기

--- src/test/java/hello/querydsl/QuerydslBasicTest.java
+++ src/test/java/hello/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package hello.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -656,6 +657,45 @@ public class QuerydslBasicTest {
             Integer rank = (Integer) tuple.get(rankPath);
 
             System.out.println("username = " + username + ", age = " + age + ", rank = " + rank);
+        }
+    }
+
+    /**
+     * 상수
+     *
+     * Expressions.constant(str) 사용
+     */
+    @Test
+    public void constant() {
+
+        List<Tuple> result = queryFactory
+                                .select(member.username, Expressions.constant("A"))
+                                .from(member)
+                                .fetch();
+
+        for (Tuple tuple : result) {
+
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 문자 더하기 concat
+     *
+     *  문자가 아닌 다른 타입들은 stringValue() 로 문자로 변환 가능
+     */
+    @Test
+    public void concat() {
+
+        List<String> result = queryFactory
+                                .select(member.username.concat("_").concat(member.age.stringValue()))
+                                .from(member)
+                                .where(member.username.eq("member1"))
+                                .fetch();
+
+        for (String s : result) {
+
+            System.out.println("s = " + s);
         }
     }
 }
