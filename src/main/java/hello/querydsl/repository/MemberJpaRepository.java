3ec1ac33dc477d5983e12c1882e9d2143cbc6adf
LEE HYO JEONG <hyojeong1913@naver.com>
2022-04-26 00:05:32 +0900

    동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- src/main/java/hello/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/hello/querydsl/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package hello.querydsl.repository;
 
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import hello.querydsl.dto.MemberSearchCondition;
 import hello.querydsl.dto.MemberTeamDto;
@@ -142,5 +144,72 @@ public class MemberJpaRepository {
                 .leftJoin(member.team, team)
                 .where(builder)
                 .fetch();
+    }
+
+    /**
+     * Where 절 파라미터 사용
+     *
+     * @param condition
+     * @return
+     */
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    /**
+     * where 절에 파라미터 방식을 사용하면 조건 재사용 가능
+     *
+     * @param condition
+     * @return
+     */
+    public List<Member> searchMember(MemberSearchCondition condition) {
+
+        return queryFactory
+                .selectFrom(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+
+        return ageLoe != null ? member.age.goe(ageLoe) : null;
     }
 }
