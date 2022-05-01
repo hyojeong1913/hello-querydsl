3b1aee6be7d4a0a9aa885e2c8e0cbffd847f2747
LEE HYO JEONG <hyojeong1913@naver.com>
2022-05-02 00:03:15 +0900

    스프링 데이터 페이징 활용3 - 컨트롤러 개발

--- src/main/java/hello/querydsl/controller/MemberController.java
+++ src/main/java/hello/querydsl/controller/MemberController.java
@@ -3,7 +3,10 @@ package hello.querydsl.controller;
 import hello.querydsl.dto.MemberSearchCondition;
 import hello.querydsl.dto.MemberTeamDto;
 import hello.querydsl.repository.MemberJpaRepository;
+import hello.querydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +17,7 @@ import java.util.List;
 public class MemberController {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 조회
@@ -25,5 +29,17 @@ public class MemberController {
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
 
         return memberJpaRepository.search(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+
+        return memberRepository.searchPageComplex(condition, pageable);
     }
 }
